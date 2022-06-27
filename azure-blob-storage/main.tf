216a88a3ae596a89ff4484c2e3fc4e4e701e4340
Plastuszko <92673556+Plastuszko@users.noreply.github.com>
2022-06-27 12:56:08 +0200

    Update main.tf

--- azure-blob-storage/main.tf
+++ azure-blob-storage/main.tf
@@ -1,1 +1,26 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg"
+  location = "West Europe"
+}
+resource "azurerm_storage_account" "sa" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.sa.minions
+  location                 = azurerm_resource_group.sa.WestEurope
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
+  tags = {
+    environment = "staging"
+  }
+resource "azurerm_storage_container" "sc" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.sc.sc
+  container_access_type = "private"
+}
+resource "azurerm_storage_blob" "sb" {
+  name                   = "some_content.zip"
+  storage_account_name   = azurerm_storage_account.sb.minions_v2
+  storage_container_name = azurerm_storage_container.sb.container
+  type                   = "Block"
+  source                 = "file.zip"
+}
